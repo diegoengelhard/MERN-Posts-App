31ea3a48520c7d0d241e02faa91f5983c7d6e0c7
Diego Engelhard <00356119@uca.edu.sv>
2024-01-25 10:56:24 -0600

    Add: API auth middleware

--- posts-app-api/routes/api/posts.router.js
+++ posts-app-api/routes/api/posts.router.js
@@ -4,20 +4,23 @@ const router = express.Router();
 // Imports Post Controller
 const postController = require('../../controllers/post.controller');
 
+// Imports Auth Middleware
+const { authenticate } = require('../../middlewares/auth.middleware');
+
 // Defines all routes
 // GET Get all posts
 router.get('/getPosts', postController.getPosts);
 
 // POST Create new post
-router.post('/createPost', postController.createPost);
+router.post('/createPost', authenticate, postController.createPost);
 
 // PATCH Update post
-router.patch('/:id', postController.updatePost);
+router.patch('/:id', authenticate, postController.updatePost);
 
 // DELETE Delete post
-router.delete('/:id', postController.deletePost);
+router.delete('/:id', authenticate, postController.deletePost);
 
 // PATCH Like post
-router.patch('/:id/likePost', postController.toggleLike);
+router.patch('/:id/likePost', authenticate, postController.toggleLike);
 
 module.exports = router;
